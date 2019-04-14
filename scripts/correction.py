0c9a72a4e453372cdd85724e5c83a4062865f13f
Doug V <dougmvieira@gmail.com>
2019-04-14 17:50:25 +0100

    Correction series computed eliminating start and end of session

--- scripts/correction.py
+++ scripts/correction.py
@@ -3,6 +3,7 @@ from argparse import ArgumentParser
 import numpy as np
 import pandas as pd
 
+from align_settings import STARTTIME, ENDTIME
 from utils import autorreg_correction
 
 
@@ -23,8 +24,11 @@ if __name__ == '__main__':
     cli.add_argument('dest_filename')
     args = cli.parse_args()
 
-
     bbo = pd.read_parquet(args.src_filename)
-    bbo_corr = get_correction(bbo)
 
+    times = bbo.index.get_level_values('Time')
+    mask = (STARTTIME <= times) & (times <= ENDTIME)
+    mask[:-3] |= mask[3:]
+
+    bbo_corr = get_correction(bbo[mask])
     bbo_corr.to_parquet(args.dest_filename, coerce_timestamps='us')
